5b03ae75ae5f74734d628fbf07125630bba1c451
ytyaru <yttry0@gmail.com>
2022-05-24 20:47:21 +0900

    Created a repository

---
+++ docs/js/main.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', async(event) => {
+// SVGを使うときに知っておくといいことをまとめました
+// https://qiita.com/manabuyasuda/items/01a76204f97cd73ffc4e
+    const baseUrl = `./asset/image/sns/`
+    let res = await fetch(`${baseUrl}list.tsv`)
+    let text = await res.text()
+    const tableH = new ImageTableHorizon()
+    //const monaBtn = new MonaButton()
+    console.log(text)
+    document.getElementById(`image-table-horizon`).innerHTML = tableH.make(baseUrl, text)
+
+    const mention = new WebMention(30) 
+    await mention.make() 
+    //document.getElementById(`content`).innerHTML = markdown.parse(document.getElementById(`markdown`).innerText)
+    //console.log(monaBtn.make(baseUrl, text))
+    //document.getElementById(`throw-mona-buttons`).innerHTML = monaBtn.make(baseUrl, text)
+    /*
+    window["markdown"].ready.then(async(markdown) => {
+        res = await fetch(`index.md`)
+        text = await res.text()
+        document.getElementById(`content`).innerHTML = markdown.parse(text)
+        hljs.highlightAll();
+    })
+    */
+});
+
